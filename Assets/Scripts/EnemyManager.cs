85971cac280f51d43b9a3b3097b3c83a04aee594
AIsacson <annaisacson@Annas-MacBook-Pro.local>
2017-07-12 16:19:44 +0200

    In progress

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+
+	public GameObject enemy;
+	public float spawnTime = 10f;
+	public Transform[] spawningPoints;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+	}
+
+	void Spawn () {
+		int spawnPointIndex = Random.Range (0, spawningPoints.Length);
+		if (GameObject.FindGameObjectsWithTag("Enemy").Length < 5) {
+			Instantiate (enemy, spawningPoints [spawnPointIndex].position, spawningPoints [spawnPointIndex].rotation);
+		}
+	}
+}
